45ae5af6b9b272e60599832196574b4fa8083c3a
Naresh Kamineni <kamieninaresh68@gmail.com>
2025-07-29 11:35:25 +0530

    skills section added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Projects from './components/Projects';
 import Education from './components/Education';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
 import CustomNavbar from './components/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Experience from './components/Experience';
